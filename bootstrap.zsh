c317d2815e1cd554f6bbc3401d22007bb8b09792
bleen <git.119x@bleen.net>
2013-12-30 13:28:32 -0500

    Adding xdebug and xhprof settings

--- bootstrap.zsh
+++ bootstrap.zsh
@@ -15,6 +15,7 @@ function doIt () {
   sudo cp -av .lamp/httpd.conf /etc/apache2/
   sudo cp -av .lamp/httpd-vhosts.conf /etc/apache2/extra/
   sudo cp -av .lamp/php.ini /usr/local/etc/php/5.4/
+  sudo cp -av .lamp/conf.d /usr/local/etc/php/5.4/
   sudo cp -av .lamp/my.cnf /etc/mysql/
   sudo cp -av .lamp/hosts /etc/
   echo "\n"
