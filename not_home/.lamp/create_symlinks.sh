382657d56701bd103fc44c736b1b097ac3d969f6
bleen <git.119x@bleen.net>
2015-04-08 22:29:03 -0400

    updating script to reflect local apache and php55 only

--- not_home/.lamp/create_symlinks.sh
+++ not_home/.lamp/create_symlinks.sh
@@ -2,14 +2,14 @@
 
 # httpd.conf #
 ##############
-mv /usr/local/etc/apache2/2.4/httpd.conf /usr/local/etc/apache2/2.4/httpd.conf.orig
-ln -s $HOME/.lamp/apache/httpd.conf /usr/local/etc/apache2/2.4/httpd.conf
+mv /private/etc/apache2/httpd.conf /private/etc/apache2/httpd.conf.orig
+ln -s $HOME/.lamp/apache/httpd.conf /private/etc/apache2/httpd.conf
 echo "Symlinked the apache 'httpd.conf' file"
 
 # apache extra #
 ################
-mv /usr/local/etc/apache2/2.4/extra /usr/local/etc/apache2/2.4/extra.orig
-ln -s $HOME/.lamp/apache/extra /usr/local/etc/apache2/2.4/extra
+mv /private/etc/apache2/extra /private/etc/apache2/extra.orig
+ln -s $HOME/.lamp/apache/extra /private/etc/apache2/extra
 echo "Symlinked the apache 'extra' directory"
 
 # mysql my.cnf #
@@ -20,19 +20,15 @@ echo "Symlinked the mysql 'my.cnf' file"
 
 # php php.ini #
 ###############
-mv /usr/local/etc/php/5.4/php.ini /usr/local/etc/php/5.4/php.ini.orig
 mv /usr/local/etc/php/5.5/php.ini /usr/local/etc/php/5.5/php.ini.orig
-ln -s $HOME/.lamp/php54/php.ini /usr/local/etc/php/5.4/php.ini
 ln -s $HOME/.lamp/php55/php.ini /usr/local/etc/php/5.5/php.ini
-echo "Symlinked the php 'php.ini' files (php5.4 & php5.5)"
+echo "Symlinked the php 'php.ini' files (php5.5)"
 
 # php conf.d #
 ###############
-mv /usr/local/etc/php/5.4/conf.d /usr/local/etc/php/5.4/conf.d.orig
 mv /usr/local/etc/php/5.5/conf.d /usr/local/etc/php/5.5/conf.d.orig
-ln -s $HOME/.lamp/php54/conf.d /usr/local/etc/php/5.4/conf.d
 ln -s $HOME/.lamp/php55/conf.d /usr/local/etc/php/5.5/conf.d
-echo "Symlinked the php 'conf.d' folders (php5.4 & php5.5)"
+echo "Symlinked the php 'conf.d' folders (php5.5)"
 
 # hosts #
 #########
