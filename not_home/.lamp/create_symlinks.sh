489dbc125552de24a15e3fd520e88dc84aa6029e
bleen <git.119x@bleen.net>
2016-02-03 09:08:31 -0500

    Ensure that we do not override the originals

--- not_home/.lamp/create_symlinks.sh
+++ not_home/.lamp/create_symlinks.sh
@@ -5,37 +5,38 @@ sudo -v
 
 # httpd.conf #
 ##############
-mv /private/etc/apache2/httpd.conf /private/etc/apache2/httpd.conf.orig
+mv -n /private/etc/apache2/httpd.conf /private/etc/apache2/httpd.conf.orig
 ln -s $HOME/.lamp/apache/httpd.conf /private/etc/apache2/httpd.conf
 sudo mkdir /usr/local/var/run/apache2
 echo "Symlinked the apache 'httpd.conf' file and created /usr/local/var/run/apache2"
 
 # apache extra #
 ################
-mv /private/etc/apache2/extra /private/etc/apache2/extra.orig
+mv -n /private/etc/apache2/extra /private/etc/apache2/extra.orig
 ln -s $HOME/.lamp/apache/extra /private/etc/apache2/extra
 echo "Symlinked the apache 'extra' directory"
 
 # mysql my.cnf #
 ################
-mv /etc/mysql/my.cnf /etc/mysql/my.cnf.orig
+sudo mkdir /etc/mysql
+mv -n /etc/mysql/my.cnf /etc/mysql/my.cnf.orig
 ln -s $HOME/.lamp/mysql/my.cnf /etc/mysql/my.cnf
 echo "Symlinked the mysql 'my.cnf' file"
 
 # php php.ini #
 ###############
-mv /usr/local/etc/php/5.5/php.ini /usr/local/etc/php/5.5/php.ini.orig
+mv -n /usr/local/etc/php/5.5/php.ini /usr/local/etc/php/5.5/php.ini.orig
 ln -s $HOME/.lamp/php55/php.ini /usr/local/etc/php/5.5/php.ini
 echo "Symlinked the php 'php.ini' files (php5.5)"
 
 # php conf.d #
 ###############
-mv /usr/local/etc/php/5.5/conf.d /usr/local/etc/php/5.5/conf.d.orig
+mv -n /usr/local/etc/php/5.5/conf.d /usr/local/etc/php/5.5/conf.d.orig
 ln -s $HOME/.lamp/php55/conf.d /usr/local/etc/php/5.5/conf.d
 echo "Symlinked the php 'conf.d' folders (php5.5)"
 
 # hosts #
 #########
-mv /etc/hosts /etc/hosts.orig
+mv -n /etc/hosts /etc/hosts.orig
 ln -s $HOME/.lamp/hosts /etc/hosts
 echo "Symlinked the 'hosts' file"
